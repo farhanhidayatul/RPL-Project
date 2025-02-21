135385ed5763e78898377e48dfb6a23beadba8b4
Farhan Hidayatul Haq <farhanhidayatul2002@gmail.com>
2025-02-21 12:55:48 +0700

    add

--- src/App.js
+++ src/App.js
@@ -3,15 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactGA from 'react-ga4';
 import Loading from './pages/loading';
 import Home from './pages/home';
-import Game from './pages/game';
-import Tutorial from './pages/tutorial';
-import About from './pages/about';
 import NF from './pages/notfound';
-import InGame from "./component/component_game/inGame";
 import GamePage from "./pages/gamePage";
-import Login from "./pages/login"
-import Register from "./pages/register"
-import Result from "./pages/result";
 
 const TRACKING_ID = "G-FLNMFT2FES"; // Your Google Analytics tracking ID
 ReactGA.initialize(TRACKING_ID);
@@ -45,14 +38,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Loading />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/game" element={<Game />} />
-        <Route path="/tutorial" element={<Tutorial />} />
-        <Route path="/about" element={<About />} />
         <Route path="/gamePage" element={<GamePage />} />
-        <Route path="/inGame" element={<InGame/>} />
-        <Route path="/result" element={<Result/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
         <Route path="*" element={<NF />} />
 
       </Routes>
